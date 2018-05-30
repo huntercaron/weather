54bb207cf3d2dd7012dbd0f8a06366824f3780a1
Hunter Caron <huntercaron@mac.com>
2018-05-30 14:22:17 -0400

    starting to work on grabbing data with the api

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -2,6 +2,21 @@ import React, { Component } from 'react';
 import './main.css';
 
 class Main extends Component {
+  state = {
+    weather: {}
+  }
+
+  componentDidMount() {
+    fetch("https://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=967218598dbdf5cdc2bfece78c2438ae")
+      .then(response => {
+        console.log(response);
+        return response.json()
+      })
+      .then(response => {
+        console.log(response);
+      })
+  }
+
   render() {
     return (
       <div>
